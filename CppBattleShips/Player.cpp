c5946de61bea390cc493d6468eec4d6731bbc82f
Nasir Muhammad <nmuhammad.nmf@gmail.com>
2018-10-25 18:14:40 +0100

    changes to player method getValidCinput
    i've change the method so that the player now has to enter row coordinates starting from 1 rather than 0.
    
    next task is to implement this method into the "guessShip" method to make it more efficent.

--- CppBattleShips/Player.cpp
+++ CppBattleShips/Player.cpp
@@ -6,25 +6,40 @@ int Player::getValidCinput(string axis)
 {
 	int coordinate;
 
-	cout << "\nEnter the desired " << axis << ":" << endl; //get input for one of the possible axis of coordinates.
-	coordinate = getValidIntInput();
+	cout << "\nEnter the desired " << axis << ":" << endl; //Get input for one half of one of the set of coordinates.
+	coordinate = getValidIntInput();	
+
+	while (coordinate < 1) //while loop to validate input of numbers which are less than 1(we do this because we want the user to give coordinates starting from 1.
+	{
+		cout << "\n" << axis << " coordinate cannot be less than 1, please re-enter: " << endl;
+		coordinate = getValidIntInput();
+	}
+
+	//decrement AFTER checking if the input is below 1, as 1 is decremented to 0 and caught by the loop, ruling out placement of ships on row 1.
+	coordinate -= 1; //decrement by 1 so that the user can give a row and column number starting from 1.
 
 	//if the axis specifed was "row", check to see if the row number exists/is on the board. if it doesn't, loop input until it does.
 	if (axis == "row")
 	{
+		
+		//make sure the player can't reference a row that doesn't exist on the row(which could either be above the size of the row/column or below 0.
 		while (coordinate > board.size()-1)
 		{
 			cout << "\nRow does not exist within the board, please enter a different row." << endl;
 			coordinate = getValidIntInput();
+			coordinate-=1; //decrement player input again so that they can always enter coordinates starting from 1.
 		}
 	}
 
-	else if (axis == "column")
+	//we could easily reference the number of  rows, as they are the same, but to be more specific we reference the amount of columns in each row.
+	//similar algorithim for validation of "row"
+	else if (axis == "column") 
 	{
-		while (coordinate > board[0].size() - 1)
+		while (coordinate > board[0].size()-1)
 		{
 			cout << "Column does not exist within the board, please enter a different column." << endl;
 			coordinate = getValidIntInput();
+			coordinate-=1;
 		}
 	}
 
@@ -55,6 +70,7 @@ void Player::placeShip()
 
 }
 
+//check whether this function works efficently or not.
 vector<int>Player::guessShip(int boardSize)
 {
 
